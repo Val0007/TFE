6152635847ccd78f33c8951ebc96d38e4ee12a2e
Val0007 <63280664+Val0007@users.noreply.github.com>
2023-10-22 06:53:39 +0530

    change4 for deployment

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -53,11 +53,16 @@ export async function getStaticProps(params:SlugType2) {
 // Your component code here to render the blog post
 export default function BlogPost({ post}:any) {
     // Render the blog post content here using the "post" data
+
+    console.log(post)
+    console.log("blocks are ",post[0].blocks)
+
     return (
       <div>
         <NavBar page="slug"></NavBar>
       <div className="md:flex md:flex-col md:justify-center md:items-center">
       <div className="w-screen px-4 md:px-0 text-sm md:w-1/2 text-left md:text-lg">
+
         <PortableText
         value={post[0].blocks || []}
         components={RichTextComponents}
